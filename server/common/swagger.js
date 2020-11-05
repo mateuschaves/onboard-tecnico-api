d6952c574268b250e4aa32aa7840312fcec3e9a5
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-11-05 15:44:14 -0300

    sonar setup

--- server/common/swagger.js
+++ server/common/swagger.js
@@ -1,6 +1,5 @@
 import middleware from 'swagger-express-middleware';
 import * as path from 'path';
-import errorHandler from '../api/middlewares/error.handler';
 
 export default function swagger(app, routes) {
   return new Promise((resolve, reject) => {
